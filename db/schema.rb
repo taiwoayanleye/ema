99226d693131f13383f52403db0decff016f1893
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-16 14:19:51 +0100

    Generate and asscociated company_profile_id to job_postings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016131455) do
+ActiveRecord::Schema.define(version: 20141016131822) do
 
   create_table "company_profiles", force: true do |t|
     t.string   "company_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141016131455) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_id"
+    t.integer  "company_profile_id"
   end
 
   create_table "skills", force: true do |t|
