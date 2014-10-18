dd53e430ade1f42940243c297eb8320f4d63eab8
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-18 14:39:02 +0100

    Add image tag to student profile

--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -2,6 +2,8 @@ class CompanyProfile < ActiveRecord::Base
 	#ASSOCIATIONS
 	has_one :user, as: :profileable, dependent: :destroy
 	accepts_nested_attributes_for :user
-
 	has_many :job_postings
+
+	# Used for image uploading
+	mount_uploader :image, ImageUploader
 end
--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -2,4 +2,7 @@ class StudentProfile < ActiveRecord::Base
 	#ASSOCIATIONS
 	has_one :user, as: :profileable, dependent: :destroy
 	accepts_nested_attributes_for :user
+
+	# Used for image uploading
+	mount_uploader :image, ImageUploader
 end
