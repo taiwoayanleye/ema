3f5191af322b041a3b1f3f5644e1b8a352940a5b
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-20 04:50:30 +0100

    Add Skills controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,30 +1,64 @@
 class SkillsController < ApplicationController
   before_action :set_skill, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!, :get_user
+  #keep user form accessing thier profile if they haven't created it yet
+  before_filter :profile_redir
+  #keep user form accessing any method that isn't connected to their profile
+  before_filter {
+    |c|
+    if Skill.exists?(params[:id])
+      c.deny_access(Skill.find(params[:id].to_i).student_profile_id)
+    else
+      if c.get_profile_type != "student"
+        c.deny_access(-1)
+      end
+    end
+  }
+  #redirect company if they haven't been verified
+  before_filter :verified?
+
   # GET /skills
   # GET /skills.json
-  def index
-    @skills = Skill.all
-  end
+  # def index
+  #   @skills = Skill.all
+  # end
 
   # GET /skills/1
   # GET /skills/1.json
   def show
+    @skill = Skill.find(params[:id])
+
+    respond_to do |format|
+      format.html #show.html.erb
+      format.json { render json: @skill }
+    end
   end
 
   # GET /skills/new
+  # GET /skills/new/.json
   def new
     @skill = Skill.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @skill }
+    end
   end
 
   # GET /skills/1/edit
   def edit
+    @skill = Skill.find(params[:id])
   end
 
   # POST /skills
   # POST /skills.json
   def create
-    @skill = Skill.new(skill_params)
+    # @skill = Skill.new(skill_params)
+    @skill = Skill.new(params[:skill])
+    @user = current_user
+    @skill.user_id = @user.id
+    @skill.student_profile_id = @user.id
 
     respond_to do |format|
       if @skill.save
@@ -40,12 +74,16 @@ class SkillsController < ApplicationController
   # PATCH/PUT /skills/1
   # PATCH/PUT /skills/1.json
   def update
+    @skill = Skill.find(params[:id])
+
     respond_to do |format|
-      if @skill.update(skill_params)
-        format.html { redirect_to @skill, notice: 'Skill was successfully updated.' }
-        format.json { render :show, status: :ok, location: @skill }
+      # if @skill.update(skill_params)
+      if @skill.update_attributes(params[:skill])
+        format.html { redirect_to student_profile_url(@user), notice: 'Skill was successfully updated.' }
+        format.json { head :no_content }
+        # format.json { render :show, status: :ok, location: @skill }
       else
-        format.html { render :edit }
+        format.html { render action: "edit" }
         format.json { render json: @skill.errors, status: :unprocessable_entity }
       end
     end
@@ -54,9 +92,11 @@ class SkillsController < ApplicationController
   # DELETE /skills/1
   # DELETE /skills/1.json
   def destroy
+    @skill = Skill.find(params[:id])
     @skill.destroy
+
     respond_to do |format|
-      format.html { redirect_to skills_url, notice: 'Skill was successfully destroyed.' }
+      format.html { redirect_to skills_url(@user), notice: 'Skill was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
