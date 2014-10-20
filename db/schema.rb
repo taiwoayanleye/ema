b4af7a8b8caf23b6ce83530640de77d46eeaedcb
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-20 11:10:17 +0100

    Add company profile id to saved student profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020100343) do
+ActiveRecord::Schema.define(version: 20141020100842) do
 
   create_table "company_profiles", force: true do |t|
     t.string   "company_name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20141020100343) do
     t.string   "skill_text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_profile_id"
   end
 
   create_table "skills", force: true do |t|
