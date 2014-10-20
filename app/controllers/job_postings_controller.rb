f40bcb43cc73d8b84e4f377c478aa2b25337368c
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-20 19:22:03 +0100

    Add search controller and view

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -28,6 +28,131 @@ class JobPostingsController < ApplicationController
     end
   end
 
+  def search()
+
+    @job_postings_all = JobPosting.all
+    @saved = SavedJobPosting.find_all_by_student_profile_id(@user.id)
+    @return = []
+    @cultures = ['']
+    Groups.all.each do |culture|
+      @cultures.push(culture.description)
+    end
+    @pay = ['', 'Paid', 'Unpaid']
+
+    #@params = params[:student_profile_id] = @user.id
+    if params[:save_search]
+      #@saved_job_posting = SavedJobPosting.new(params[:saved_job_posting])
+      @saved_job_posting = SavedJobPosting.new
+      #@saved_job_posting.student_profile_id = params[:student_profile_id]
+      @saved_job_posting.student_profile_id = @user.id
+      @saved_job_posting.position_text = params[:position_text]
+      @saved_job_posting.description_text = params[:description_text]
+      @saved_job_posting.paid_text = params[:paid_text]
+      @saved_job_posting.requirements_text = params[:requirements_text]
+      @saved_job_posting.culture = params[:culture]
+
+      respond_to do |format|
+        if @saved_job_posting.save
+          format.html { redirect_to :back, notice: 'Saved job posting was successfully created.' }
+          format.json { render json: @saved_job_posting, status: :created, location: @saved_job_posting }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @saved_job_posting.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    if params[:position_text]
+      if params[:position_text] != "" and params[:description_text] != "" and params[:paid_text] != "" and params[:requirements_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term3 = params[:paid_text]
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_description LIKE ? AND Job_paid = ? AND Job_requirements LIKE ?", match_term1, match_term2, match_term3, match_term4)
+      elsif params[:position_text] != "" and params[:description_text] != "" and params[:paid_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term3 = params[:paid_text]
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_description LIKE ? AND Job_paid = ?", match_term1, match_term2, match_term3)
+      elsif params[:position_text] != "" and params[:description_text] != "" and params[:requirements_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_description LIKE ? AND Job_requirements LIKE ?", match_term1, match_term2, match_term4)
+      elsif params[:position_text] != "" and params[:paid_text] != "" and params[:requirements_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term3 = params[:paid_text]
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_paid = ? AND Job_requirements LIKE ?", match_term1, match_term3, match_term4)
+      elsif params[:description_text] != "" and params[:paid_text] != "" and params[:requirements_text] != ""
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term3 = params[:paid_text]
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Job_description LIKE ? AND Job_paid LIKE ? AND Job_requirements LIKE ?", match_term2, match_term3, match_term4)
+      elsif params[:position_text] != "" and params[:description_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term2 = "%" + params[:description_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_description LIKE ?", match_term1, match_term2)
+      elsif params[:position_text] != "" and params[:paid_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term3 = params[:paid_text]
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_paid = ?", match_term1, match_term3)
+      elsif params[:position_text] != "" and params[:requirements_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ? AND Job_requirements LIKE ?", match_term1, match_term4)
+      elsif params[:description_text] != "" and params[:paid_text] != ""
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term3 = params[:paid_text]
+        @job_postings = JobPosting.where("Job_description LIKE ? AND Job_paid = ?", match_term2, match_term3)
+      elsif params[:description_text] != "" and params[:requirements_text] != ""
+        match_term2 = "%" + params[:description_text] + "%"
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Job_description LIKE ? AND Job_requirements LIKE ?", match_term2, match_term4)
+      elsif params[:paid_text] != "" and params[:requirements_text] != ""
+        match_term3 = params[:paid_text]
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Job_paid = ? AND Job_requirements LIKE ?", match_term3, match_term4)
+      elsif params[:position_text] != ""
+        match_term1 = "%" + params[:position_text] + "%"
+        @job_postings = JobPosting.where("Position LIKE ?", match_term1)
+      elsif params[:description_text] != ""
+        match_term2 = "%" + params[:description_text] + "%"
+        @job_postings = JobPosting.where("Job_description LIKE ?", match_term2)
+      elsif params[:paid_text] != ""
+        match_term3 = params[:paid_text]
+        @job_postings = JobPosting.where("Job_paid = ?", match_term3)
+      elsif params[:requirements_text] != ""
+        match_term4 = "%" + params[:requirements_text] + "%"
+        @job_postings = JobPosting.where("Job_requirements LIKE ?", match_term4)
+      end
+
+      if params[:culture] != ''
+        #culture is set but there are no search hits
+        if @job_postings.nil?
+          @job_postings_all.each do |profile|
+            if CompanyProfile.find(profile.company_profile_id).qsort == params[:culture]
+              @return.append(profile)
+            end
+          end
+          #culture is set and there are search hits
+        else
+          @job_postings.each do |profile|
+            if CompanyProfile.find(profile.company_profile_id).qsort == params[:culture]
+              @return.append(profile)
+            end
+          end
+        end
+        #no culture is set
+      else
+        @return = @job_postings
+      end
+    else
+      #when the search page is initially visited, displays all of the job postings
+      #@return = @job_postings_all
+    end
+  end
+
   # GET /job_postings/1
   # GET /job_postings/1.json
   def show
--- app/controllers/student_profiles_controller.rb
+++ app/controllers/student_profiles_controller.rb
@@ -21,6 +21,99 @@ class StudentProfilesController < ApplicationController
     end
   end
 
+  def search
+    @return = []
+    @cultures = ['']
+    Groups.all.each do |culture|
+      @cultures.push(culture.description)
+    end
+    @years = ['', 'Freshman', 'Sophomore', 'Junior', 'Senior', 'N/A']
+
+    @student_profiles_all = StudentProfile.all
+    @saved = SavedStudentProfile.find_all_by_company_profile_id(@user.id)
+
+    if params[:save_search]
+      @saved_student_profile = SavedStudentProfile.new
+      @saved_student_profile.company_profile_id = @user.id
+      @saved_student_profile.school_text = params[:school_text]
+      @saved_student_profile.year_text = params[:year_text]
+      @saved_student_profile.skill_text = params[:skill_text]
+      @saved_student_profile.culture = params[:culture]
+
+      respond_to do |format|
+        if @saved_student_profile.save
+          format.html { redirect_to :back, notice: 'Saved student profile was successfully created.' }
+          format.json { render json: @saved_student_profile, status: :created, location: @saved_student_profile }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @saved_student_profile.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    if params[:school_text]
+      if params[:school_text] != "" and params[:year_text] != "" and params[:skill_text] != ""
+        match_term1 = params[:school_text]
+        match_term2 = params[:year_text]
+        match_term3 = params[:skill_text]
+        @student_profiles = StudentProfile.find_by_sql("SELECT * FROM Student_Profiles sp INNER JOIN skills s ON  sp.id = s.student_profile_id WHERE s.description LIKE '%" +
+                                                           match_term3 + "%' AND sp.school LIKE '%" + match_term1 + "%' AND sp.school_year LIKE '%" + match_term2 + "%'" )
+      elsif params[:school_text] != "" and params[:skill_text] != ""
+        match_term1 =  params[:school_text]
+        match_term2 =  params[:skill_text]
+        @student_profiles = StudentProfile.find_by_sql("SELECT * FROM Student_Profiles sp INNER JOIN skills s ON  sp.id = s.student_profile_id WHERE s.description LIKE '%" +
+                                                           match_term2 + "%' AND sp.School LIKE '%" + match_term1 + "%'")
+      elsif params[:year_text] != "" and params[:skill_text] != ""
+        match_term1 =  params[:year_text]
+        match_term2 =  params[:skill_text]
+        @student_profiles = StudentProfile.find_by_sql("SELECT * FROM Student_Profiles sp INNER JOIN skills s ON  sp.id = s.student_profile_id WHERE s.description LIKE '%" +
+                                                           match_term2 + "%' AND sp.school_year LIKE '%" + match_term1 + "%'")
+      elsif params[:year_text] != "" and params[:school_text] != ""
+        match_term1 = "%" + params[:year_text] + "%"
+        match_term2 = "%" + params[:school_text] + "%"
+        @student_profiles = StudentProfile.where("School_year LIKE ? AND School LIKE ?", match_term1, match_term2)
+      elsif params[:year_text] != ""
+        match_term = "%" + params[:year_text] + "%"
+        @student_profiles = StudentProfile.where("School_year LIKE ?", match_term)
+      elsif params[:school_text] != ""
+        match_term = "%" + params[:school_text] + "%"
+        @student_profiles = StudentProfile.where("School LIKE ?", match_term)
+      elsif params[:skill_text] != ""
+        match_term1 = params[:skill_text]
+        @student_profiles = StudentProfile.find_by_sql("SELECT * FROM Student_Profiles sp INNER JOIN skills s ON  sp.id = s.student_profile_id WHERE s.description LIKE '%" + match_term1 + "%'")
+      end
+
+      if params[:culture] != ''
+        #culture is set but there are no search hits
+        if @student_profiles.nil?
+            @student_profiles_all.each do |profile|
+              if profile.qsort == params[:culture]
+                @return.append(profile)
+              end
+            end
+        #culture is set and there are search hits
+        else
+          @student_profiles.each do |profile|
+            if profile.qsort == params[:culture]
+              @return.append(profile)
+            end
+          end
+        end
+      #culture is not set
+      else
+        @return = @student_profiles
+      end
+    else
+      #when the search page is initially visited, display all interns
+      #@return = @student_profiles_all
+    end
+    @school_text = params[:school_text]
+    @year_text = params[:year_text]
+    @skill_text = params[:skill_text]
+    @culture = params[:culture]
+
+  end
+
   # GET /student_profiles/1
   # GET /student_profiles/1.json
   def show
