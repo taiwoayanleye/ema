5ced636a50d2e7b12931069e6adc4f32f89d1a7c
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-22 16:36:41 +0100

    Add devise confirmable for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
   # Needed for Devise gem as the defined default options for url
   # Should set to the actual host of your application
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #Devise mail verification
+  config.action_mailer.perform_deliveries = true
+  config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'ariesdevelop@gmail.com',
+    :password             => 'aries@@develop',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
