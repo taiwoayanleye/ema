031815eb1831eccee29b9bb28abbdfa72459a5af
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-27 14:58:16 +0000

    Set S3 keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 #AWS configuration with carrierwave
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => "AWS",                        # required
-    :aws_access_key_id      => ENV["AKIAJXLBWTLWLMYLRAFQ"],                        # required
-    :aws_secret_access_key  => ENV["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"],                       # required
+    :provider               = "AWS",                        # required
+    :aws_access_key_id      = ENV["AKIAJXLBWTLWLMYLRAFQ"],                        # required
+    :aws_secret_access_key  = ENV["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"],                       # required
   }
   config.fog_directory  = ENV["stuternbeta"]                          # required
 end
