7bc9635ef7a878188182a556720d9a1f07f49bfa
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-04 12:48:10 +0000

    Add company profile id to job applications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104080759) do
+ActiveRecord::Schema.define(version: 20141104124716) do
 
   create_table "company_profiles", force: true do |t|
     t.string   "company_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141104080759) do
   create_table "job_applications", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_profile_id"
   end
 
   create_table "job_postings", force: true do |t|
