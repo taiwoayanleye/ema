a3eef7dab246275b226ab9805e9bd6f90b7a17cb
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-05 18:05:15 +0000

    Specify cover letter storage

--- app/uploaders/cover_letter_uploader.rb
+++ app/uploaders/cover_letter_uploader.rb
@@ -7,9 +7,13 @@ class CoverLetterUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # encoding
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
