a87220d46db47858e554dc4d2c19572453335729
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-11 09:20:53 +0000

    Add mandrill setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,11 @@ Rails.application.configure do
   config.assets.debug = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'ariesdevelop@gmail.com',
-    :password             => 'aries@@develop',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+    :address              => "smtp.mandrillapp.com",
+    :domain               => 'mandrillapp.com',
+    :user_name            => ENV["MANDRILL_USERNAME"],
+    :password             => ENV["MANDRILL_APIKEY"],
+    :authentication       => 'plain'
+  }
 end
